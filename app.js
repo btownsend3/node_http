4113b3481ba121fa30a9934269504db7e90f785e
Brett Townsend <brett@Bretts-Air.lan>
2022-03-19 12:04:04 -0500

    added sample website pages and simple http server

---
+++ app.js
@@ -0,0 +1,38 @@
+const http = require('http')
+const fs = require('fs')
+
+const port = 8080
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    fs.readFile('index.html', (err, data) => {
+      if (err) throw err
+      res.writeHead(200, {"Content-Type": 'text/html'})
+      res.write(data)
+      return res.end()
+    })
+  } else if (req.url === '/about') {
+    fs.readFile('about.html', (err, data) => {
+      if (err) throw err
+      res.writeHead(200, {"Content-Type": 'text/html'})
+      res.write(data)
+      return res.end()
+    })
+  } else if (req.url === '/contact') {
+    fs.readFile('contact.html', (err, data) => {
+      if (err) throw err
+      res.writeHead(200, {"Content-Type": 'text/html'})
+      res.write(data)
+      return res.end()
+    })
+  } else {
+    fs.readFile('404.html', (err, data) => {
+      if (err) throw err
+      res.writeHead(200, {"Content-Type": 'text/html'})
+      res.write(data)
+      return res.end()
+    })
+  }
+})
+
+server.listen(port)
